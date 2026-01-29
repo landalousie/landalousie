f5161b2545a8ec0253bf4d9a3b01688c6edf075e
daniel.sanchez <daniel.sanchez@lemoncode.net>
2026-01-29 20:23:02 +0100

    refactor: replace DEPLOY_URL with SITE_URL in environment constants and update related references

--- src/core/constants/env.constants.ts
+++ src/core/constants/env.constants.ts
@@ -1,5 +1,5 @@
 export const ENV = {
   CONTENT_ISLAND_ACCESS_TOKEN: process.env.CONTENT_ISLAND_ACCESS_TOKEN || '',
   STRIPE_API_KEY: process.env.STRIPE_API_KEY || '',
-  DEPLOY_URL: process.env.DEPLOY_URL || '',
+  SITE_URL: process.env.SITE_URL || '',
 };
